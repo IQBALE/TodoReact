407b0cc12da7e812983ff135e72a5b14db4d2b12
IQBALE <iqbal.haswan@gmail.com>
2024-08-29 15:35:28 +0800

    finished

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,18 +2,22 @@ import { useState } from "react"
 import style from './form.module.css'
 
 export default function Form({ todos, setTodos }) {
-    const [todo, setTodo] = useState("");
+    // const [todo, setTodo] = useState("");
+    const [todo, setTodo] = useState({name: "", done:false});
+
 
     function handleSubmit(e) {
         e.preventDefault();
         setTodos([...todos, todo]);
-        setTodo("");
+        setTodo({name: "", done:false});
     }
 
     return (
         <form className={style.todoForm} onSubmit={handleSubmit}>
-            <input className={style.todoInput} onChange={(e) => setTodo(e.target.value)} type="text" value={todo} placeholder="Enter Todo Data"></input>
-            <button type="submit">Add</button>
+            <div className={style.inputContaner}>
+                <input className={style.todoInput} onChange={(e) => setTodo({name:e.target.value, done:false})} type="text" value={todo.name} placeholder="Enter Todo Item"></input>
+                <button className={style.todoButton} type="submit">Add</button>
+            </div>
         </form>
     );
 }
